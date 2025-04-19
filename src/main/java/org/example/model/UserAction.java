3a73fa83482f6bd32d927fc46483223c86202300
ArtemSmit21 <smarde2103@mail.ru>
2025-04-20 01:36:05 +0300

    Update UserAction.java

--- src/main/java/org/example/model/UserAction.java
+++ src/main/java/org/example/model/UserAction.java
@@ -1,28 +1,15 @@
 package org.example.model;
-
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Value;
-
-import java.time.Instant;
-
-@Value
-public class UserAction {
-  long id;
-  Instant eventTime;
-  String eventType;
-  String eventDetails;
-
-  @JsonCreator
-  public UserAction(
-    @JsonProperty("id") long id,
-    @JsonProperty("eventTime") Instant eventTime,
-    @JsonProperty("eventType") String eventType,
-    @JsonProperty("eventDetails") String eventDetails
-  ) {
-    this.id = id;
-    this.eventTime = eventTime;
-    this.eventType = eventType;
-    this.eventDetails = eventDetails;
-  }
-}
+ 
+ import lombok.AllArgsConstructor;
+ import lombok.Getter;
+ 
+ import java.time.Instant;
+ 
+ @AllArgsConstructor
+ @Getter
+ public class UserAction {
+   private long id;
+   private Instant eventTime;
+   private String eventType;
+   private String eventDetails;
+ }
